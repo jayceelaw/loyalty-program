8d877a3c0f2f62802036e61360b77bce4fa59728
root <root@chris.localdomain>
2025-11-21 20:57:52 -0500

    created feedback message component and modified global styles

---
+++ course-project/frontend/src/app/components/FeedbackMessage.js
@@ -0,0 +1,7 @@
+'use client';
+
+// message [string]: the content of the message
+// error [boolean]: whether it is an error or success message 
+export default function FeedBackMessage({message, error}) {
+    return <p className={`${message === '' ? 'hidden' : 'message'} ${error ? "error" : "success"}`}>{message}</p>
+}
