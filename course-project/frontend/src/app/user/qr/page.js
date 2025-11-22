735c8cafa9ac2adb04f0bf8433c1abce1a92880d
root <root@chris.localdomain>
2025-11-21 21:01:25 -0500

    display user qr and utorid

--- course-project/frontend/src/app/user/qr/page.js
+++ course-project/frontend/src/app/user/qr/page.js
@@ -1,17 +1,14 @@
 'use client';
 import QRCode from "react-qr-code";
-import styles from '@/app/transaction/transfer.css';
-import { useState } from "react";
-
+import { useAuth } from "@/context/AuthContext";
 
 export default function UserQR({userID}) {
-
-    const [ utorid, setUtorid ] = useState("abcd123");
+    const { user } = useAuth();
 
     return (
         <div className="main-container">
             <QRCode className="qr" value={{userID: userID, type: 'user'}} />
-            <h2>{`utorid:  ${utorid}`} <br/>
+            <h2>{`utorid:  ${user ? `${user.utorid}` : "Loading..."}`} <br/>
                 {`Scan QR to initiate a transaction.`}</h2>
         </div>
     );
