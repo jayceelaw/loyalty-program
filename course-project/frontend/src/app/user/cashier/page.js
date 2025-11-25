60a27f967a4fe5b931af1274a9abe337ac362117
LavenderLO <lolavender528@gmail.com>
2025-11-24 22:40:07 -0500

    Modified cashier landing page logic

--- course-project/frontend/src/app/user/cashier/page.js
+++ course-project/frontend/src/app/user/cashier/page.js
@@ -4,20 +4,26 @@ import { useAuth } from "@/context/AuthContext";
 import { PrimaryButton } from "../../components/Button";
 import styles from '../user.module.css';
 
+const showAdvancedFeatures = true; // Toggle to false if no features added 
+
 export default function CashierDashboardPage() {
   const router = useRouter();
   const { user } = useAuth();
   if (!user) return <p>Loading...</p>;
 
+  const cardClassName = showAdvancedFeatures ? `${styles.resultsCard} ${styles.modifiedCashierResultsCard}` : `${styles.resultsCard} ${styles.fullHeightCashierResultsCard}`;
+
   return (
-    <div className={styles.pageContainer}>
-      <main className={`${styles.resultsCard} ${styles.modifiedCashierResultsCard}`}>
-        <h2 className={styles.welcome}>Welcome, {user.name}!</h2>
-        <div className={styles.buttons}>
-          <PrimaryButton text="Create Transaction" onClick={() => router.push("/transaction/purchase")} />  {/* Not sure if this is the right page for create transactions*/}
+    <div className={styles.pageContainer}> 
+      <main className={cardClassName}>
+        <h2 className={styles.welcome}>Welcome, {user.name}!</h2> 
+        <div className={showAdvancedFeatures ? styles.buttons : styles.largeButtons}>
+          <PrimaryButton text="Create Transaction" onClick={() => router.push("/transaction/purchase")} />  {/*page for create transactions*/}
           <PrimaryButton text="Process Redemption" onClick={() => router.push("/transaction/process")} />
         </div>
-        <div className={styles.cashierDataSection}><p>Space reserved for advanced features; if anyone wants to add data visualization stuff, go for it :D</p></div>
+        {showAdvancedFeatures && (<div className={styles.cashierDataSection}><p>TODO: Space reserved for advanced features; if anyone wants to add data visualization stuff, go for it :D</p>
+        </div>
+        )}
       </main>
     </div>
   );
